8d6b929f5d1009807fb229cbd3020c3aa7a47cf0
Rita6333 <161160146+Rita6333@users.noreply.github.com>
2024-03-02 02:21:27 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Rita`, 
+  name: `Noblefu Media Team🚨`, 
   type: ActivityType.LISTENING }), 10000);
 client.errorLog = config.errorLog
 } else {
