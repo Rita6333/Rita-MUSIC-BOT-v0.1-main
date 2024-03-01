b30b467ead462fe218ab37445c2b187c3ecf9c83
Rita6333 <161160146+Rita6333@users.noreply.github.com>
2024-03-02 02:24:15 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,7 +23,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `Noblefu Media Team🚨`, 
-  type: ActivityType.LISTENING }), 10000);
+  type: ActivityType.PLAYINGA }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
